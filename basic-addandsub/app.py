2c837a5cfb7c36e035ee675226c60bd178e5d42e
gopisrinivaskanna <gopisrinivaskanna@gmail.com>
2026-08-18 20:10:19 +0530

    arranging by folders

---
+++ basic-addandsub/app.py
@@ -0,0 +1,5 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter second number"))
+print(f"sum of {num1} and {num2} is {num1+num2}")
+print(f"substraction of {num1} and {num2} is {num1+num2}")
+
