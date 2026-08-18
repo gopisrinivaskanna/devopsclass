308bf8b97404118efd51d8150c31c4d7f23bf062
gopisrinivaskanna <gopisrinivaskanna@gmail.com>
2026-08-18 20:00:08 +0530

    added substraction and updated addition

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 num1=int(input("enter first number:"))
 num2=int(input("enter second number"))
-print(num1+num2)
+print(f"sum of {num1} and {num2} is {num1+num2}")
+print(f"substraction of {num1} and {num2} is {num1+num2}")
+
